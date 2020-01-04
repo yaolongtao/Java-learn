cb12db45210913129439ea4f8819e6e6550f65d8
yaolongtao <yaolongtao@shinow.com>
2020-01-04 11:39:15 +0800

    创建demo和javaEE_web模块

---
+++ demo/src/Hello_world.java
@@ -0,0 +1,9 @@
+/**
+ * @author yaolongtao
+ * @create 2020-01-04-11:33
+ */
+public class Hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
