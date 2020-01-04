bd5d94be3d7dc78848c7c5d09f3bbf815623d87d
yaolongtao <yaolongtao@shinow.com>
2020-01-04 14:48:46 +0800

    添加gradletest

--- demo/src/HelloWorld.java
+++ demo/src/HelloWorld.java
@@ -2,7 +2,7 @@
  * @author yaolongtao
  * @create 2020-01-04-11:33
  */
-public class Hello_world {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
